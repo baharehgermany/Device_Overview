35a4a81dcf5c3c0bd651a30359f37b75cdc1464c
Bhareh Etemadi <baharehetemadi81@gmail.com>
2022-01-06 14:01:46 +0100

    first work

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="main">
+
+        <div class="top">
+<div class="headline" ><h1>Device Overview</h1></div>
+        </div>
+
+        <div class="nav"></div>
+        <div class="list">
+            <div class="dev_table">
+                <div class="dev_title">
+                    <div class="clm1">host</div>
+                    <div class="clm2">ip</div>
+                    <div class="clm3">mac</div>
+                    <div class="clm4">user</div>
+                    <div class="clm5">status</div>
+                </div>
+                <div class="dev_row">
+                    <div class="clm1">host</div>
+                    <div class="clm2">ip</div>
+                    <div class="clm3">mac</div>
+                    <div class="clm4">user</div>
+                    <div class="clm5">status</div>
+                </div>
+                <div class="dev_row">
+                    <div class="clm1">host</div>
+                    <div class="clm2">ip</div>
+                    <div class="clm3">mac</div>
+                    <div class="clm4">user</div>
+                    <div class="clm5">status</div>
+                </div>
+                <div class="dev_row">
+                    <div class="clm1">host</div>
+                    <div class="clm2">ip</div>
+                    <div class="clm3">mac</div>
+                    <div class="clm4">user</div>
+                    <div class="clm5">status</div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+</body>
+</html>
